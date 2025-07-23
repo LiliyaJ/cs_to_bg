0bdca76497403b8e9c86936893eda1cc4586fed2
Liliya Jeromin <zl108108@gmail.com>
2025-07-23 18:53:25 +0200

    deploy

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ def handle_request():
         traceback.print_exc()
         return make_response(json.dumps({"error": str(e)}), 500)
     
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8080))
+#     app.run(host="0.0.0.0", port=port)
